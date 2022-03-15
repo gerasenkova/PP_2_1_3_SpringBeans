9dbb5aed4115eef42a86a68c0ce70ec2cea9c09f
Angelina Gerasenkova <gerasenlova2012@mail.ru>
2022-03-15 17:07:21 +0300

    version2

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -6,11 +6,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-    @Autowired
-    @Qualifier("dog")
+
+
     private Animal animal;
-    @Autowired
     private Timer timer;
+
+    @Autowired
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
